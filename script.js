82f9cae10820e1135d5bd26d618897d7c939b511
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-03-12 01:29:41 +0700

    fix

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Inisialisasi AOS
-    AOS.init();
+    AOS.init({
+        duration: 1000,
+        once: true,
+    });
 
     gsap.from(".hero h1", { opacity: 0, y: -50, duration: 1 });
     gsap.from(".hero p", { opacity: 0, y: 50, duration: 1, delay: 0.5 });
+    gsap.from(".section-title", { opacity: 0, y: -50, duration: 1, delay: 0.3 });
 
     // Inisialisasi Typed.js
     var typed = new Typed(".typed-text", {
