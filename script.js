c90d995daf6041f70f4214d500c1cb6be0218072
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-03-12 00:46:57 +0700

    update error gsap

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-AOS.init();
+document.addEventListener("DOMContentLoaded", function() {
+    // Inisialisasi AOS
+    AOS.init();
 
-var typed = new Typed(".typed-text", {
-    strings: ["Web Developer", "Gamer", "DevOps Enginer"],
-    typeSpeed: 100,
-    backSpeed: 50,
-    loop: true
+    gsap.from(".hero h1", { opacity: 0, y: -50, duration: 1 });
+    gsap.from(".hero p", { opacity: 0, y: 50, duration: 1, delay: 0.5 });
+
+    // Inisialisasi Typed.js
+    var typed = new Typed(".typed-text", {
+        strings: ["Web Developer", "DevOps Engineer", "Content Creator"],
+        typeSpeed: 100,
+        backSpeed: 50,
+        loop: true
+    });
 });
-
-gsap.from(".hero h1", { opacity: 0, y: -50, duration: 1 });
-gsap.from(".hero p", { opacity: 0, y: 50, duration: 1, delay: 0.5 });
-
